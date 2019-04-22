4b5c89fa310ef8dd49adc46b97d5705a0d81bbf8
hcm <chaominghu1996@163.com>
2019-04-22 21:22:44 +0800

    第二次尝试上传

--- src/main/java/com/baizhi/entity/Emp.java
+++ src/main/java/com/baizhi/entity/Emp.java
@@ -4,4 +4,13 @@ public class Emp {
     private String id;
     private String name;
     private Integer age;
+
+    public Emp(String id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Emp() {
+    }
 }
