113baebfa2329dae5887c480376e01e98978a98d
wangz <wangzhe03011997@hotmail.com>
2019-04-23 09:03:36 +0800

    Editing entity

--- src/main/java/com/baizhi/entity/Emp.java
+++ src/main/java/com/baizhi/entity/Emp.java
@@ -1,16 +1,40 @@
 package com.baizhi.entity;
 
 public class Emp {
-    private Integer id;
+    private String id;
     private String name;
     private Integer age;
 
-    public Emp(Integer id, String name, Integer age) {
+    public Emp(String id, String name, Integer age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
 
     public Emp() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
