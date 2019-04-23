ff838e7aa7bd1dcea793886d2f2868ecefb49694
wangz <wangzhe03011997@hotmail.com>
2019-04-23 08:39:52 +0800

    First collaborative development from group 5

--- src/main/java/com/baizhi/entity/Emp.java
+++ src/main/java/com/baizhi/entity/Emp.java
@@ -1,11 +1,11 @@
 package com.baizhi.entity;
 
 public class Emp {
-    private String id;
+    private Integer id;
     private String name;
     private Integer age;
 
-    public Emp(String id, String name, Integer age) {
+    public Emp(Integer id, String name, Integer age) {
         this.id = id;
         this.name = name;
         this.age = age;
